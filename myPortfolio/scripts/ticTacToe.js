7ea8d7fe9bd88bc721a71742ea6f2a91f3d5c433
JiJiLopez <jiovahnymartinez@gmail.com>
2019-03-27 20:49:03 -0500

    updated

--- myPortfolio/scripts/ticTacToe.js
+++ myPortfolio/scripts/ticTacToe.js
@@ -1,12 +1,16 @@
-//setup the game board
-//clear all x's and o's from the board
-//decide who mkaes the first move We can assume player x always makes the first move
+var gameMarker = "X";
 
-//player clicks a spot on thee board, mark the board if it is unoccupied, else give an error message
-//check if the game is finish after marking the board
-//if the game is finished then output indicating the winner
-//if the game is not finished the other players turn
-//repeats until the game is finished or it is a draw
-//prompt players to decide if they want to play a new game
-//if yes, go back to step 1
+function changeMarkerToX(){
+    gameMarker = "x";
+    // console.log("The X button was clicked!")
+}
 
+function changeMarkerToO(){
+    gameMarker = "O";
+    // console.log("The O button was clicked!")
+}
+
+function placeMark(divID){
+    var box = document.getElementById(divID);
+    box.innerHTML = gameMarker;
+}
